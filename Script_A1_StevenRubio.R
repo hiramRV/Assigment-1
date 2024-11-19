74d2f45340c67cbc50d1e3d3672f6ee7b04f69ed
HiramRubio <stevenrv97@gmail.com>
2024-11-19 17:55:14 +0100

    Update
    #Question 1
    - Calculated Skewness
    #Question 6
    - Multiple linear models using interactions

--- Script_A1_StevenRubio.R
+++ Script_A1_StevenRubio.R
@@ -5,6 +5,9 @@
 library(openxlsx)
 library(mosaic)
 library(RColorBrewer)
+library(DescTools)
+library(e1071)
+
 data <-read.xlsx("dataset01.xlsx")
 
 ### Pre-processing ----
@@ -19,6 +22,8 @@ summary(data$STARTING_PRICE)
 #histogram
 histogram(~STARTING_PRICE, data=data, breaks=20, main="Distribution of Starting price", 
           ylim=c(0,30), xlab = "Starting Price", ylab = "Count")  #Right-skew
+skewness(data$STARTING_PRICE)
+
 #Boxplot
 boxplot(data$STARTING_PRICE) #A lot of possible outlier 
 
@@ -33,12 +38,11 @@ barplot(dist2,col=colors()[c(23,89,12,112,200)], beside=T,,ylim=c(0,50))
 data_percentage <- apply(dist1, 2, function(x){x*100/sum(x,na.rm=T)})
 
 # Make a stacked barplot--> it will be in %!
-barplot(data_percentage, col=coul , border="white", xlab="group")
+#barplot(data_percentage, col=coul , border="white", xlab="group")
 
 ### Question 3: Analyze Region and Area ----
 histogram(~ AREA | REGION, data = data)
 boxplot(AREA ~ REGION, data = data)
-
 
 ### Question 4: Analyze STARTING_PRICE and AREA ----
 #We see that they are positively related
@@ -75,11 +79,26 @@ abline(simple_reg, col = "#FF1F06")
 # Fit linear regression between STARTING_PRICE and REGION,TYPE,BALCONY,ROOMS and AREA
 data_num <- select(data,ROOMS,AREA,STARTING_PRICE)
 plot(data_num)
+data$STARTING_PRICE <-data$STARTING_PRICE/ 1000000
 
-multiple_reg = lm(STARTING_PRICE~REGION+TYPE+ROOMS+AREA+BALCONY, data=data)
-summary(multiple_reg)
-abline(multiple_reg, col = "#488606")
+multiple_reg = lm(STARTING_PRICE~REGION+TYPE+AREA+BALCONY+ROOMS, data=data)
+summary(multiple_reg) #0.5498 - 0.5439
 
+multiple_reg2 = lm(STARTING_PRICE~REGION+TYPE+AREA+BALCONY+ROOMS+REGION*AREA, data=data)
+summary(multiple_reg2)
+
+multiple_reg3 = lm(STARTING_PRICE~REGION+TYPE+AREA+BALCONY+ROOMS+ TYPE*AREA, data=data)
+summary(multiple_reg3)
+
+multiple_reg4 = lm(STARTING_PRICE~REGION+TYPE+AREA+BALCONY+ROOMS+REGION*AREA+TYPE*AREA, data=data)
+summary(multiple_reg4)
+
+multiple_reg5 = lm(STARTING_PRICE~REGION+TYPE+AREA, data=data)
+summary(multiple_reg5)
+
+multiple_reg6 = lm(STARTING_PRICE~REGION+TYPE+AREA+REGION*AREA+TYPE*AREA, data=data)
+summary(multiple_reg6)
+round(multiple_reg6$coefficients, 3)
 
 ### Question 7: Predict housing starting price ----
 test <-read.xlsx("test.xlsx")
@@ -89,11 +108,10 @@ test$TYPE<-factor(test$TYPE)
 test$BALCONY<-factor(test$BALCONY)
 View(data)
 
-predict(multiple_reg,test)   #Predict using our model
+predict(multiple_reg6,test)   #Predict using our model
 # Our Original data
 data[data$ID %in% c(629,718,1534,1695,1864,2138),]
 
 #Test Area
 test_area <- select(test,AREA)
-predict(simple_reg,test)   #Predict using the simpe model from Q4
-
+predict(simple_reg,test)   #Predict using the simple model from Q4
